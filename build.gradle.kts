7b91716be11db22fc17653ad9cab30c7f632e44b
Kir <dev.temp.null.mail@gmail.com>
2025-10-04 06:32:17 +0400

    inc version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.kirv.plugin"
-version = "0.0.2"
+version = "0.0.3"
 
 val platformType = providers.gradleProperty("platformType").orElse("IC")
 val platformVersion = providers.gradleProperty("platformVersion").orElse("2025.2")
